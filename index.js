56d679be03e29b6ba053d52513d46354632a68f4
Olejekglejek <negrutaoleg@gmail.com>
2021-01-11 21:21:36 +0000

    rearranging code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
- 
 function navSlide() {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
   var topbutton = document.getElementById("topBtn");
       const bibi = document.querySelector(".bibi");
-
 //topButton
 window.onscroll = function() {scrollFunction()};
 
@@ -16,7 +14,6 @@ function scrollFunction() {
     topbutton.style.display = "none";
   }
 }
-
     burger.addEventListener("click", () => {
         //Toggle Nav
         nav.classList.toggle("nav-active");
@@ -29,7 +26,6 @@ function scrollFunction() {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
-     
         //Burger Animation
         burger.classList.toggle("toggle");
     })
@@ -45,34 +41,17 @@ function scrollFunction() {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
-     
         //Burger Animation
         burger.classList.toggle("toggle");
     })
-       
-
- 
-    
-  
          document.querySelector('.theme-toggle-btn').addEventListener('click', () => {
         document.body.classList.toggle('purple')
     });
-    
 }
-// functionnavSlideBack(){
-//   const nav = document.querySelector(".nav-links");
-//   const navLinks = document.querySelectorAll(".nav-links li");
-  
-//   burger.addEventListener("click", () => {
-//     //Toggle Nav
-//     nav.classList.toggle("nav-active");
-//   });
-// }
 const app = () => {
     navSlide();
 }
 app()
-
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
